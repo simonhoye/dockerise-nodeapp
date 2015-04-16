257ac0fbd9364d44a537788882aa39cf75ed9b49
Simon Hoye <simonhoye@gmail.com>
2015-04-16 12:06:38 +1000

    text update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+  res.send('This docker thingy is pretty cool\n');
 });
 
 app.listen(PORT);
